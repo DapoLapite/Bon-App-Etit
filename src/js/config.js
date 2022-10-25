3ea96687b471f508896ab69426ac82d163d0b21a
Dapo Lapite <104777764+DapoLapite@users.noreply.github.com>
2022-10-25 10:38:37 -0400

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ebeae37d-bff1-49ff-80a8-a323764304e9';
+export const MODAL_CLOSE_SEC = 2.5;
